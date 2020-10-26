9acbe4dea34e38bdbe90e3fdb44c426490f20cef
tdt0088 <tdt008@163.com>
2020-10-26 14:51:12 +0800

    #add: 创建springboot demo module

---
+++ spring-boot-demo/src/main/java/com/tdt/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.tdt.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: Application
+ *
+ * @date: 2020年10月26日 14:38
+ * @author: qinrenchuan
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
